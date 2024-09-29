56c6026fb1a70b29d608944a8b9c51dcaca2c47d
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-09-29 18:58:54 +0300

    Complete the function pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    let sentence = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < num; i++) {
+        sentence += str
+    }
+    return sentence;
 };
 
 // Do not edit below this line
