a00757a6f62cd466c787a04b49d09ba4d0d49150
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-09-29 20:11:59 +0300

    Comlete the function, Pass All tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+    const arr = args[0];
+    let len = arr.length;
+    let argLen = args.length;
+    let found = false;
+    let newArr = [];
+    for (let i =0; i < len; i++)
+    {
+        found = false;
+        for (let j = 1; j< argLen; j++)
+            {
+                if(arr[i] === args[j])
+                    {
+                        found = true;
+                    }
+            }
+            if (!found)
+                {
+                    newArr.push(arr[i]);
+                } 
+    }
+    return newArr;
 };
 
 // Do not edit below this line
