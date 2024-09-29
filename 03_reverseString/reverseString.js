aa049fb1d00460cd7ba47ebbaffd91ded743ddf6
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-09-29 19:47:26 +0300

    Complete the function, Pass All Tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    let len = string.length;
+    for (let i = len - 1; i >= 0; i--)
+    {
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
