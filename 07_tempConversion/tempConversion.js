d3fb0401a54a6b161c16d35a59130a243c7c8b1f
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-09-30 14:47:04 +0300

    Complete Function, Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  // Fahrenheit to Celsius Formula
+  let celsiusTemp = (fahrenheitTemp - 32) * (5/9);
+  if (Number.isInteger(celsiusTemp)){
+    return +celsiusTemp;
+  }
+ return +celsiusTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  // Celsius To Fahrenheit Formula
+  let fahrenheitTemp = (celsiusTemp * (9/5)) + 32;
+  if (Number.isInteger(fahrenheitTemp)){
+    return +fahrenheitTemp;
+  }
+  return +fahrenheitTemp.toFixed(1);
 };
 
 // Do not edit below this line
