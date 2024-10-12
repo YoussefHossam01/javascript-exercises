48671c256ec274ff093f79d2dde06d691b2cd02b
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-10-12 18:04:34 +0300

    Add all required implementations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let number in arr){
+    sum += arr[number];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let number in arr){
+    result *= arr[number];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return Math.pow(n1, n2);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n <= 1){
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
